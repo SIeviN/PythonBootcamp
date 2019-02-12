a7bb96282017a701c82fc8b4f6599c493534be43
SIeviN <michaelkochd@gmail.com>
2019-02-12 14:53:11 -0600

    Update 03 Function Practice Exercises.py

--- 03 Function Practice Exercises.py
+++ 03 Function Practice Exercises.py
@@ -103,5 +103,106 @@ print('has_33([1, 3, 3]) = ', has_33([1, 3, 3]))
 print('has_33([1, 3, 1, 3]) = ', has_33([1, 3, 1, 3]))
 print('has_33([3, 1, 3]) = ', has_33([3, 1, 3]))
 
+#######################################################################################################################################################################
+print(' ')
+print('8.')
+print('PAPER DOLL: Given a string, return a string where for every character in the original there are three characters')
+#######################################################################################################################################################################
 
+def paper_doll(text):
+    newstring = ''
+    for i in text:
+        newstring += i*3
+    return newstring
 
+print('paper_doll(Hello) = ', paper_doll('Hello'))
+print('paper_doll(Mississippi) = ', paper_doll('Mississippi'))
+
+#######################################################################################################################################################################
+print(' ')
+print('9.')
+print('BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum.')
+print('If their sum exceeds 21 and theres an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return BUST')
+#######################################################################################################################################################################
+
+def blackjack(a,b,c):
+    tot = a+b+c
+    if tot <= 21:
+        return tot
+    elif tot > 21:
+        if a == 11 or b == 11 or c == 11:
+            tot -= 10
+            if tot > 21:
+                return 'BUST'
+            else:
+                return tot
+        else:
+            return 'BUST'
+
+print('blackjack(5,6,7) = ', blackjack(5,6,7))
+print('blackjack(9,9,9) = ', blackjack(9,9,9))
+print('blackjack(9,9,11) = ', blackjack(9,9,11))
+
+#######################################################################################################################################################################
+print(' ')
+print('10.')
+print('SUMMER OF 69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and')
+print('extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.')
+#######################################################################################################################################################################
+
+def summer_69(arr):
+    sixflag = 0
+    tot = 0
+    for i in arr:
+        if sixflag == 0 and i != 6 and i != 9:
+            tot += i
+        elif i == 6:
+            sixflag = 1
+            continue
+        elif i == 9 and sixflag == 1:
+            sixflag = 0
+            continue
+    return tot
+
+print('summer_69([1,3,5]) = ', summer_69([1,3,5]))
+print('summer_69([4, 5, 6, 7, 8, 9]) = ', summer_69([4, 5, 6, 7, 8, 9]))
+print('summer_69([2, 1, 6, 9, 11]) = ', summer_69([2, 1, 6, 9, 11]))
+
+#######################################################################################################################################################################
+print(' ')
+print('11.')
+print('SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order')
+#######################################################################################################################################################################
+
+def spy_game(nums):
+    fzero = 0
+    szero = 0
+    for i in nums:
+        if i == 0 and fzero == 0:
+            fzero = 1
+        elif i == 0 and fzero == 1 and szero == 0:
+            szero = 1
+        elif i == 7 and fzero == 1 and szero == 1:
+            return True
+    return False
+
+print('spy_game([1,2,4,0,0,7,5]) = ', spy_game([1,2,4,0,0,7,5]))
+print('spy_game([1,0,2,4,0,5,7]) = ', spy_game([1,0,2,4,0,5,7]))
+print('spy_game([1,7,2,0,4,5,0]) = ', spy_game([1,7,2,0,4,5,0]))
+
+#######################################################################################################################################################################
+print(' ')
+print('12.')
+print('COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number')
+#######################################################################################################################################################################
+
+def count_primes(num):
+    count = 0
+    for i in range(2,101):
+        if i%2 != 0 and i%3 != 0 and i%5 !=0 and i%7 != 0:
+            count += 1
+        if i == 2 or i == 3 or i == 5 or i == 7:
+            count += 1
+    return count
+
+print('count_primes(100) = ', count_primes(100))
